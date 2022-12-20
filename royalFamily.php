42cf7e432f051e404c8236b574ab97caa4154285
jaldabaoth-code <grialazurabi@gmail.com>
2022-12-20 06:54:36 +0100

    save

--- royalFamily.php
+++ royalFamily.php
@@ -1,41 +1,35 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
         <title>The kingdom of Colchis - Royal Family</title>
-        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet" />
-        <link href="assets/styles/style.css" rel="stylesheet" type="text/css" />
-        <link href="assets/styles/royalFamily.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+        <link href="assets/styles/style.css" rel="stylesheet" type="text/css">
+        <link href="assets/styles/royalFamily.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <?php
             /* header included */
             include 'header.html';
         ?>
-        <div class="content">
-            <h1>Members of the Royal House</h1>
-            <div class="content">
-                <section class="royals">
-                    <div class="flex-container">
-                        <div class="flex-item">
-                            <img src="assets/images/aeetis.jpg" alt="King Aeetis" />
-                            <h3>King Aeetis</h3>
-                            <p>The son of Sun god Helios and the Oceanid Perseis.</p>
-                        </div>
-                        <div class="flex-item">
-                            <img src="assets/images/medea.jpg" alt="Princess Medea" />
-                            <h3>Princess Medea</h3>
-                            <p>A priestess of the goddess Hecate.</p>
-                        </div>
-                        <div class="flex-item">
-                            <img src="assets/images/idyia.jpg" alt="Queen Idyia" />
-                            <h3>Queen Idyia</h3>
-                            <p>The youngest of the Oceanides.</p>
-                        </div>
-                    </div>
-                </section>
+        <h1>Members of the Royal House</h1>
+        <section class="royals">
+            <div class="royal">
+                <img src="assets/images/aeetis.jpg" alt="King Aeetis" />
+                <h3>King Aeetis</h3>
+                <p>The son of Sun god Helios and the Oceanid Perseis</p>
             </div>
-        </div>
+            <div class="royal">
+                <img src="assets/images/medea.jpg" alt="Princess Medea" />
+                <h3>Princess Medea</h3>
+                <p>A priestess of the goddess Hecate</p>
+            </div>
+            <div class="royal">
+                <img src="assets/images/idyia.jpg" alt="Queen Idyia" />
+                <h3>Queen Idyia</h3>
+                <p>The youngest of the Oceanides</p>
+            </div>
+        </section>
     </body>
 </html>
